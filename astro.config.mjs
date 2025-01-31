d5feb784675419127bae4ca2c7048d6b91356397
Joy2225 <archishman9892@gmail.com>
2025-02-01 00:08:53 +0530

    wrong link issue fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://irregular-25.vercel.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
